d096fa0e5f103142927ae12951dab686883d5c48
Xin Zhi <115723006+xong002@users.noreply.github.com>
2023-07-18 18:44:26 +0800

    add Game model

--- src/main/java/practice/day28workshop/model/Game.java
+++ src/main/java/practice/day28workshop/model/Game.java
@@ -1,5 +1,8 @@
 package practice.day28workshop.model;
 
+import java.util.Date;
+import java.util.List;
+
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -8,5 +11,14 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 @AllArgsConstructor
 public class Game {
-    
+    private int id;
+    private String name;
+    private int year;
+    private int rank;
+    private int usersRated;
+    private String url;
+    private String thumbnail;
+    private List<String> reviews;
+    private Date timestamp;
+
 }
---
+++ src/main/java/practice/day28workshop/service/GameService.java
@@ -0,0 +1,8 @@
+package practice.day28workshop.service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class GameService {
+    
+}
